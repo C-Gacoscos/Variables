7f752c2e1e45281ac3d1ac83c23807f129e44b04
unknown <ceedriclazaro010205@gmail.com>
2024-09-06 22:06:58 -0800

    Initialize commit with app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log("Hello, World!");
+let name = "Ceedric L. Gacoscos";
+console.log('My name is ' + name);
+
+let age = 19;
+console.log(age);
+
+let favColors = ['black', 'white', 'grey']
+console.log(favColors)
